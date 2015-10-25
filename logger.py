13cd592d54007759cd5f991b6e068bc60e1a0cd5
Bruno Thalmann <mr.thalmann@gmail.com>
2015-10-25 20:10:35 +0100

    changed to class

--- logger.py
+++ logger.py
@@ -1,19 +1,23 @@
 import hashlib
 
-kept = list()
+class Logger:
+    'A class that takes care of desktop items that needs to be kept over time'
 
-def load_kept_items():
-    with open('kept.dc', 'r') as f:
-        for line in f:
-            kept.append(line)
-    return kept
+    def __init__(self):
+        kept = list()
+        with open('kept.dc', 'r') as f:
+            for line in f:
+                kept.append(line)
 
-def save_kept_items():
-    with open('kept.dc', 'w') as f:
-        f.write('\n'.join(kept))
+    def save_kept_items():
+        with open('kept.dc', 'w') as f:
+            f.write('\n'.join(kept))
 
-def add_item(desktop_item):
-    kept.append(create_hash(desktop_item.filename))
+    def add_item(desktop_item):
+        kept.append(create_hash(desktop_item.filename))
 
-def create_hash(string):
-    return hashlib.md5(string).hexdigest()
+    def create_hash(string):
+        return hashlib.md5(string).hexdigest()
+
+    def contains(desktop_item):
+        return create_hash(desktop_item.filename) in kept
